1ed61379c60d70cd79dd2298ddd37aa5f298fdfc
murilooon <murilooon@gmail.com>
2023-11-27 00:02:38 -0300

    fix: http requests

--- src/components/AddSubject/index.js
+++ src/components/AddSubject/index.js
@@ -6,17 +6,25 @@ import Conclude from "../../assets/icon/conclude.svg";
 import Cancel from "../../assets/icon/cancel.svg";
 import Input from "../../components/Input";
 
+import client from '../../axios-config';
+
 const AddSubject = (props) => {
 
     const [name, setName] = useState("");
     const [description, setDescription] = useState("");
+
+    const createSubject = () => {
+        client.get('materias').then((response) => {
+            console.log(response.data);
+          });
+    }
 
     return (
         <div className="add-subject">
             <div className="add-subject-background" />
             <div className="add-subject-content">
                 <div className="add-subject-content-operations">
-                    <img src={Conclude} alt="Ícone de conclusão" />
+                    <img src={Conclude} alt="Ícone de conclusão" onClick={createSubject} />
                     <img src={Cancel} alt="Ícone de cancelamento" onClick={() => { props.setValue(false) }} />
                 </div>
                 <span className="add-subject-content-title">Cadastrar matéria</span>
